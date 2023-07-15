3f48ddb186eeacb0c791d51117e8a4771ae1b237
unknown <jakub.zezula@hotmail.com>
2023-07-15 22:51:19 +0200

    initial commit

---
+++ AdoNetTransaction/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+using AutoLotDAL.ConnectedLayer;
+using AutoLotDAL.Models;
+
+namespace AdoNetTransaction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("***** Simple Transaction Example *****\n");
+
+            bool throwEx = true;
+
+            Write("Do you want to throw an exception (Y or N): ");
+            var userAnswer = ReadLine();
+            if (userAnswer?.ToLower() == "n")
+            {
+                throwEx = false;
+            }
+
+            var dal = new InventoryDAL();
+            dal.OpenConnection(@"Data Source=(local)\;Integrated Security=SSPI;" + "Initial Catalog=AutoLot");
+
+            dal.ProcessCreditRisk(throwEx, 5);
+            WriteLine("Check CreditRisk table for results");
+            ReadLine();
+        }
+    }
+}
